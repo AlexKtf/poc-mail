832a794a9afec948baf0e561b77266b70d0abc91
AlexKtf <alexandre.ktifa@gmail.com>
2013-12-19 20:39:06 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PocMail::Application.config.secret_key_base = '23a0042d25cdf81658407117517c8de72c518763a60d9aa994e0e7b0400165cc356a5c05520d0d4f4cf6c2c7ae43ea698049be7b4bc192db71b9783c69d19fc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PocMail::Application.config.session_store :cookie_store, key: '_poc-mail_session'
