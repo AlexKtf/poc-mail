df58e72841f3bd7c314b9de4b1789db18c44836d
AlexKtf <alexandre.ktifa@gmail.com>
2013-12-31 16:53:38 +0100

    Setup message registration

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,10 +4,10 @@ class InboxController < ApplicationController
 
   def handle_inbound(event_payload)
     message = Message.new
-    puts event_payload.inspect
-    message.receiver_id = 2
-    message.sender_id = 1
-    message.content = 'true story'
+    payload = event_payload['msg']
+    message.sender_id = User.find_by_email(payload['from_email']).id
+    message.receiver_id = User.find_by_name(payload['headers']['To'][0..-18]).id
+    message.content = payload['text']
     message.save
   end
 end
