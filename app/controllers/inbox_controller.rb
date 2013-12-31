478112ab771043bef1557d07068469659dc50e64
AlexKtf <alexandre.ktifa@gmail.com>
2013-12-31 15:51:40 +0100

    inbound method

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,4 +1,13 @@
 class InboxController < ApplicationController
   include Mandrill::Rails::WebHookProcessor
 
+
+  def handle_inbound(event_payload)
+    message = Message.new
+    puts event_payload.inspect
+    message.receiver_id = 2
+    message.sender_id = 1
+    message.content = 'true story'
+    message.save
+  end
 end
